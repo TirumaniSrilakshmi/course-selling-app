9218214538b0adfb9ab0bef59df41a49c056440c
tirumaniharshavardhan <harshatirumani@Tirumanis-MacBook-Air.local>
2024-10-24 13:56:14 +0530

    step8-only start if db is up

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import {userRouter} from './routes/userRouter.mjs';
 import {courseRouter} from './routes/courseRouter.mjs';
 import { adminRouter } from './routes/adminRouter.mjs'; 
+require('dotenv').config();
 
 const app = express()
 app.use(express.json())
@@ -11,6 +12,11 @@ app.use('/api/v1/user', userRouter)
 app.use('/api/v1/course', courseRouter)
 
 
-app.listen(3000, () => {
-    console.log('Running in 3000 port...')
-})
+async function main(){
+    await mongoose.connect('mongodb+srv://harsha:8aWLslToAKzB2jKN@cluster.imuhp.mongodb.net/course-app-practice')
+    app.listen(3000, () => {
+        console.log('Running in 3000 port...')
+    })
+}
+
+main()
