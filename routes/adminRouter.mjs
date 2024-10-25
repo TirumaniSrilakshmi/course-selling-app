d68aabaa461d464f54df45f669308db4954bfe90
tirumaniharshavardhan <tirumaniharshavardhan@gmail.com>
2024-10-25 11:00:59 +0530

    added admin signup and signin functionalities

--- routes/adminRouter.mjs
+++ routes/adminRouter.mjs
@@ -1,29 +1,74 @@
     // admin signup, login, add course, delete course, course content
 
-    import { response, Router } from "express";
+    import { Router } from "express";
     import { AdminModel } from "../db.mjs";
     import {z} from 'zod';
+    import bcrypt from 'bcrypt';
+    import jwt from 'jsonwebtoken';
     const adminRouter = Router();
 
-    adminRouter.post('/signup', (request, response) => {
-        
-        z.object({
-            
+    adminRouter.post('/signup', async (request, response) => {
+        //input validation
+        const admin = z.object({
+            firstName: z.string().min(5).max(50),
+            lastName: z.string().min(5).max(50),
+            email: z.string().min(5).max(50).email(),
+            password: z.string().min(5).max(500)
         })
-        const firstName = request.body.username;
-        const lastName = request.body.lastName;
-        const email = request.body.email;
-        const password = request.body.password
-        
-        response.json({
-            message: 'You have signed up.'
-        })
+        //error handling
+        try{
+            const validateAdmin = admin.parse(request.body);
+            const hashedPassword = await bcrypt.hash(validateAdmin.password, 10)
+
+            const newadmin = await AdminModel.create({
+                firstName: validateAdmin.firstName,
+                lastName: validateAdmin.lastName,
+                email: validateAdmin.email,
+                password: hashedPassword
+            })
+
+            response.status(201).json({
+                message: 'Admin signed up successfully.',
+                firstName: newadmin.firstName
+            })
+
+        }catch(error){
+            console.log(error)
+            response.status(400).json({
+                message: 'Error signing up.',
+                error: error.errors || error.message
+            })
+        }
     })
 
-    adminRouter.post('/signin', (request, response) => {
-        response.json({
-            message: 'admin signin endpoint.'
-        })
+    adminRouter.post('/signin', async (request, response) => {
+        const {email, password} = request.body;
+
+        if(!email || !password){
+            return response.status(400).json({
+                message: 'Invalid email and password.'
+            })
+        }
+        try{
+            const foundAdmin = await AdminModel.findOne({email});
+            if(foundAdmin && await bcrypt.compare(password, foundAdmin.password)){
+            const token = jwt.sign({
+                id: foundAdmin._id.toString()
+            }, process.env.JWT_ADMIN_SECRET)
+
+            response.status(200).json({
+                message: 'You have signed in successfully.',
+                token: token
+            })
+            }
+        }
+        catch(error){
+            console.log(error)
+            response.status(500).json({
+                message: 'Error signing in.',
+                error: error.errors || error.message
+            })
+        }
     })
 
     // /api/v1/course/course shouldn't be there, it's /api/v1/course
