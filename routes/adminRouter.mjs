1b1632a05ca6eb8f891c69e249c10c2dc7c1729a
tirumaniharshavardhan <tirumaniharshavardhan@gmail.com>
2024-10-25 21:58:08 +0530

    added admin course edit handler, get all courses handlers

--- routes/adminRouter.mjs
+++ routes/adminRouter.mjs
@@ -8,6 +8,7 @@ import jwt from 'jsonwebtoken';
 import { adminMiddleware } from "../middlewares/adminmiddleware.mjs";
 const adminRouter = Router();
 
+//admin sign up
 adminRouter.post('/signup', async (request, response) => {
     //input validation
     const admin = z.object({
@@ -42,6 +43,7 @@ adminRouter.post('/signup', async (request, response) => {
     }
 })
 
+//admin sign in
 adminRouter.post('/signin', async (request, response) => {
     const { email, password } = request.body;
 
@@ -73,6 +75,7 @@ adminRouter.post('/signin', async (request, response) => {
 })
 
 // /api/v1/course/course shouldn't be there, it's /api/v1/course
+//admin add course
 adminRouter.post('/course', adminMiddleware, async (request, response) => {
     const adminId = request.adminId;
 
@@ -82,18 +85,18 @@ adminRouter.post('/course', adminMiddleware, async (request, response) => {
         price: z.number(),
         imageUrl: z.string().min(1).max(100)
     })
-    try{
+    try {
         const courseData = courseSchema.parse(request.body);
         const course = await CourseModel.create({
             ...courseData,
             adminId
-        }) 
+        })
         response.status(201).json({
             message: 'Course created.',
             courseId: course._id
         })
 
-    }catch(error){
+    } catch (error) {
         console.log(error);
         response.json({
             message: 'Error creating course.',
@@ -102,18 +105,58 @@ adminRouter.post('/course', adminMiddleware, async (request, response) => {
     }
 })
 
-adminRouter.put('/course', (request, response) => {
-    const adminId = request.adminId
+//admin edit course
+adminRouter.put('/course', async (request, response) => {
+    const adminId = request.adminId;
 
-    response.json({
-        message: 'admin delete courses endpoint.'
+    const courseSchema = z.object({
+        title: z.string().min(1).max(50),
+        description: z.string().min(5).max(100),
+        price: z.number().positive(),
+        imageUrl: z.string().min(1).max(100),
+        courseId: z.string().min(1)
     })
+    try {
+        const courseData = courseSchema.parse(request.body);
+        const courseMatch = await CourseModel.updateOne({ _id: courseData.courseId, adminId }, {
+            ...courseData,
+            adminId
+        })
+        if (courseMatch.matchedCount === 0) {
+            response.status(404).json({
+                message: 'Course not found.'
+            })
+        }
+        response.status(200).json({
+            message: 'Course created successfully.',
+            courseId: courseData.courseId
+        })
+
+    } catch (error) {
+        console.log(error);
+        response.json({
+            message: 'Error updating course.',
+            error: error.errors || error.message
+        })
+    }
 })
 
-adminRouter.get('/course/bulk', (request, response) => {
-    response.json({
-        message: 'admin all courses endpoint.'
-    })
+//admin get all courses as bulk
+adminRouter.get('/course/bulk', adminMiddleware, async (request, response) => {
+    const adminId = request.adminId;
+    try {
+        const bulk = await CourseModel.find()
+        response.status(200).json({
+            message: 'admin all courses endpoint.',
+            courses: bulk,
+        })
+    } catch (error) {
+        console.log(error)
+        response.status(500).json({
+            error: 'Internal server error.'
+        })
+    }
 })
 
+//export adminRouter handler
 export { adminRouter }
