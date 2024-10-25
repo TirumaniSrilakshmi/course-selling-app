fb1bea5cf97308cea98e9f7db5ffc5d8534cc1d5
tirumaniharshavardhan <tirumaniharshavardhan@gmail.com>
2024-10-25 20:45:25 +0530

    fixed the adminMiddleware issue by fixing file extensions

--- routes/adminRouter.mjs
+++ routes/adminRouter.mjs
@@ -1,11 +1,11 @@
 // admin signup, login, add course, delete course, course content
 
 import { Router } from "express";
-import { AdminModel } from "../db.mjs";
+import { AdminModel, CourseModel } from "../db.mjs";
 import { z } from 'zod';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { adminMiddleware } from "../middlewares/adminmiddleware";
+import { adminMiddleware } from "../middlewares/adminmiddleware.mjs";
 const adminRouter = Router();
 
 adminRouter.post('/signup', async (request, response) => {
@@ -74,14 +74,32 @@ adminRouter.post('/signin', async (request, response) => {
 
 // /api/v1/course/course shouldn't be there, it's /api/v1/course
 adminRouter.post('/course', adminMiddleware, async (request, response) => {
-    const { title, description, price, imageUrl, adminId } = request.body;
-    const course = await AdminModel.create({
-        title, description, price, imageUrl, adminId 
-    }) 
-    response.status(200).json({
-        message: 'Course created.',
-        courseId: course._id
+    const adminId = request.adminId;
+
+    const courseSchema = z.object({
+        title: z.string().min(1).max(50),
+        description: z.string().min(5).max(100),
+        price: z.number(),
+        imageUrl: z.string().min(1).max(100)
     })
+    try{
+        const courseData = courseSchema.parse(request.body);
+        const course = await CourseModel.create({
+            ...courseData,
+            adminId
+        }) 
+        response.status(201).json({
+            message: 'Course created.',
+            courseId: course._id
+        })
+
+    }catch(error){
+        console.log(error);
+        response.json({
+            message: 'Error creating course.',
+            error: error.errors || error.message
+        })
+    }
 })
 
 adminRouter.put('/course', (request, response) => {
